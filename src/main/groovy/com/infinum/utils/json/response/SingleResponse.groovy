41c31a17ed8af875a14982efa560222667d517ab
Daniel Bower <daniel.d.bower@gmail.com>
2013-12-20 10:12:48 -0500

    Clean up whitespace

--- src/main/groovy/com/infinum/utils/json/response/SingleResponse.groovy
+++ src/main/groovy/com/infinum/utils/json/response/SingleResponse.groovy
@@ -1,50 +1,48 @@
 package com.infinum.utils.json.response
 
 class SingleResponse<T> {
-        
-    Boolean success
-    
-    /**
-     * Payload - data affected by an action
-     */
-    T data
-    
-    /**
-     * Descriptive message to the client about the action that occurred
-     */
-    String message
+		
+	Boolean success
+	
+	/**
+	 * Payload - data affected by an action
+	 */
+	T data
+	
+	/**
+	 * Descriptive message to the client about the action that occurred
+	 */
+	String message
 
-    public SingleResponse(){
-		super()
-    }
-    
-    /**
-     * The data requested, as well as a flash message about the action.
-     */
-    public SingleResponse(T data, String message){
+	SingleResponse(){ }
+	
+	/**
+	 * The data requested, as well as a flash message about the action.
+	 */
+	SingleResponse(T data, String message){
 		super()
 		success = true
 		this.data = data
 		this.message = message
-    }
+	}
 
-    /**
-     * The data requested
-     */
-    public SingleResponse(T data){
+	/**
+	 * The data requested
+	 */
+	SingleResponse(T data){
 		super()
 		success = true
 		this.data = data
-    }
-    
-    /**
-     * In an error condition, we'll just pass back a message
-     */
-    public static SingleResponse asError(String message){
-	    return new SingleResponse<T>(success:false, message:message)
-    }
-    
-    public static jsonProperties = { SingleResponse resp ->
-        ['success': resp.success, 'data':resp.data, 'message':resp.message]
-    }   
+	}
+	
+	/**
+	 * In an error condition, we'll just pass back a message
+	 */
+	static SingleResponse asError(String message){
+		return new SingleResponse<T>(success:false, message:message)
+	}
+	
+	static jsonProperties = { SingleResponse resp ->
+		['success': resp.success, 'data':resp.data, 'message':resp.message]
+	}
 }
